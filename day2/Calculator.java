f8f6eb1009b3da1052548cc0e2bf054f67fad4ff
GirijaShivkumar <127115461+GirijaShivkumar@users.noreply.github.com>
2023-03-09 15:26:09 +0530

    Add files via upload
    Day2_Assignments

---
+++ day2/Calculator.java
@@ -0,0 +1,17 @@
+package week1.day2;
+
+public class Calculator {
+	public int add(int num1, int num2, int num3) {
+		int sum = num1+num2+num3;
+		return sum;
+	
+	}
+	public int mul(int first, int second) {
+		int multiply = first*second;
+		return multiply;
+	}
+	public void sub() {
+		System.out.println("Method with no arguments");
+	}
+	
+}
---
+++ day2/MyCalculator.java
@@ -0,0 +1,13 @@
+package week1.day2;
+
+public class MyCalculator {
+
+	public static void main(String[] args) {
+		Calculator obj = new Calculator();
+		System.out.println(obj.add(2, 5, 2));
+		System.out.println(obj.mul(5, 5));
+		obj.sub();
+
+	}
+
+}
